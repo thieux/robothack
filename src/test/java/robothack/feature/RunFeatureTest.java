82fd953f3d7361e6ca10f0d7942368f80c127efe
Mathieu Pauly <mathieu.pauly@gmail.com>
2014-05-28 11:20:15 +0200

    Strict runner.

--- src/test/java/robothack/feature/RunFeatureTest.java
+++ src/test/java/robothack/feature/RunFeatureTest.java
@@ -1,8 +1,10 @@
 package robothack.feature;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(strict = true)
 public class RunFeatureTest {
 }
